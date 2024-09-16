890e8efd867c986f465a3afe0ef9de8f364c866a
peteykienzzz <pmatczak2@gmail.com>
2024-09-16 15:27:29 -0700

    fixed the import.

--- src/pete_todoapp/app.py
+++ src/pete_todoapp/app.py
@@ -1,10 +1,10 @@
-import pete_todoapp.lib
+from lib import Task, ToDoList
 
 
 def main():
     todo_lists = {}
     todo_list = input("List name?: ")
-    todo_lists[todo_list] = pete_todoapp.lib.ToDoList(todo_list)
+    todo_lists[todo_list] = ToDoList(todo_list)
     while True:
         task = input("Task name?: ")
         task_description = input("Task description?: ")
@@ -12,7 +12,7 @@ def main():
         if task == "q":
             break
         todo_lists[todo_list].tasks.append(
-            pete_todoapp.lib.Task(task, task_description, task_due_date))
+            Task(task, task_description, task_due_date))
     todo_lists[todo_list].show_tasks()
 
 
